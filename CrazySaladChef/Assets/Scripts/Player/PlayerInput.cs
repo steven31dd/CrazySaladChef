cec086d3ce549c64a7bdeb208fab34d134eebf17
steven31dd <stevenandrade45@gmail.com>
2022-03-01 14:56:11 -0700

    update 1
    new scene, added tile sprite, created player input, removed tile script

--- CrazySaladChef/Assets/Scripts/Player/PlayerInput.cs
+++ CrazySaladChef/Assets/Scripts/Player/PlayerInput.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Keys")]
+    [SerializeField] private KeyCode _forward = KeyCode.W;
+    [SerializeField] private KeyCode _backward = KeyCode.S;
+    [SerializeField] private KeyCode _left = KeyCode.A;
+    [SerializeField] private KeyCode _right = KeyCode.D;
+    [SerializeField] private KeyCode _interaction = KeyCode.F;
+
+    Vector2 _movement;
+
+    private void Awake()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool Interact()
     {
-        
+        if (Input.GetKeyDown(_interaction)){
+            return true;
+        }
+
+        return false;
     }
+
+    public Vector2 Move()
+    {
+        _movement = Vector2.zero;
+
+        if (Input.GetKey(_forward))
+        {
+            _movement.y = 1;
+        }
+
+        if (Input.GetKey(_backward))
+        {
+            _movement.y = -1;
+        }
+
+        if (Input.GetKey(_left))
+        {
+            _movement.x = -1;
+        }
+
+        if (Input.GetKey(_right))
+        {
+            _movement.x = 1;
+        }
+
+        return _movement;
+    }
+
 }
